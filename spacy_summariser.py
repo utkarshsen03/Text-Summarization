0bc42424d78fef162f8a5a156c32a9ffedea5ea4
danom03 <utkarshsen03@gmail.com>
2024-11-21 15:16:31 +0530

    spacy updated

--- spacy_summariser.py
+++ spacy_summariser.py
@@ -3,8 +3,11 @@ import pytextrank
 
 
 class TextSummarizer:
-    def __init__(self,text):
+    def __init__(self,text,phrase_limit,sentence_limit):
         self.text = text
+        self.phrase_limit = phrase_limit
+        self.sentence_limit = sentence_limit
+    
     def summarize(self):
         nlp = spacy.load('en_core_web_md')
         nlp.add_pipe("textrank", last=True)
@@ -16,6 +19,6 @@ class TextSummarizer:
         #     print(p.chunks)
         
         summary = ''                
-        for sent in doc._.textrank.summary(limit_phrases=15, limit_sentences=5):
-            summary += sent
+        for sent in doc._.textrank.summary(limit_phrases=self.phrase_limit, limit_sentences=self.sentence_limit):
+            summary += str(sent)
         return summary
